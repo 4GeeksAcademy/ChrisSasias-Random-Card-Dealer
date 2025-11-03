9f6ba93176c45a5eb023e0e144c68fcbcee51b8b
Christopher Sasias <christophersasias@hotmail.com>
2025-11-03 20:01:34 +0000

    Create random card

--- src/app.js
+++ src/app.js
@@ -1,29 +1,22 @@
-import "bootstrap";
-import "./style.css";
-
-
-
-window.onload = function() {
-
+window.onload = function () {
   randomCard();
-  randomSticks();
- 
-
+  document.getElementById("newCardBtn").addEventListener("click", randomCard);
 };
 
 function randomCard() {
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const suits = ["♦", "♥", "♠", "♣"];
 
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
-let pokerSticks = ["♦", "♥", "♠", "♣"];
+  const randomNumber = Math.floor(Math.random() * numbers.length);
+  const randomSuit = Math.floor(Math.random() * suits.length);
+  const suit = suits[randomSuit];
+  const color = suit === "♦" || suit === "♥" ? "red" : "black";
 
+  document.querySelector(".top-suits").innerHTML = suit;
+  document.querySelector(".bottom-suits").
+  document.querySelector(".number").innerHTML = numbers[randomNumber];
 
-let randomNumbers = Math.floor(Math.random()*numbers.length)
-let randomSticks = Math.floor(Math.random()*pokerSticks.length)
-
-
-document.querySelector(".top-suits").innerHTML = pokerSticks[randomSticks];
-document.querySelector(".bottom-suits").innerHTML = pokerSticks[randomSticks];
-document.querySelector(".number").innerHTML = numbers[randomNumbers];
-
+  const card = document.querySelector(".card");
+  card.classList.remove("red");
+  if (color === "red") card.classList.add("red");
 }
-
