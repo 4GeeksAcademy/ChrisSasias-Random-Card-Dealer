59e874eb1773fb590543467c104dadf9c73a6ed3
Christopher Sasias <christophersasias@hotmail.com>
2025-11-03 19:34:24 +0000

    create basic logic and style

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  randomCard();
+  randomSticks();
+ 
+
 };
+
+function randomCard() {
+
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+let pokerSticks = ["♦", "♥", "♠", "♣"];
+
+
+let randomNumbers = Math.floor(Math.random()*numbers.length)
+let randomSticks = Math.floor(Math.random()*pokerSticks.length)
+
+
+document.querySelector(".top-suits").innerHTML = pokerSticks[randomSticks];
+document.querySelector(".bottom-suits").innerHTML = pokerSticks[randomSticks];
+document.querySelector(".number").innerHTML = numbers[randomNumbers];
+
+}
+
